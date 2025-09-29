db6cc2543c31d9847280859056d3fd11f64afdc2
zeddmork-slavik <slavik.pupkin.90@mail.ru>
2025-09-29 11:10:17 +0700

    Create board.c
    started

---
+++ sea_battle/board.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define FIELD_HEIGHT 10
+#define FIELD_WEIGHT 10
+
+int main(void) {
+    char field_for_player [FIELD_WEIGHT]{FIELD_HEIGHT} = {0}; 
+    char field_for_computer [FIELD_WEIGHT]{FIELD_HEIGHT} = {0}; 
+
+
+
+    return 0;
+}
